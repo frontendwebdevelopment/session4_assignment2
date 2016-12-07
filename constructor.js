0ccc913edfeae53ce82c65d3b0f254eb3e001011
Ajay Kumar S M <ajaymandakki@gmail.com>
2016-12-07 13:45:48 +0530

    Update constructor.js

--- constructor.js
+++ constructor.js
@@ -1,1 +1,26 @@
+class Person{
+    constructor(fname,lname){
+        this.fname = fname;
+        this.lname = lname;
+    }
+    getFullName() {
+    return this.fname + " " + this.lname;
+  }
+}
 
+class Student extends Person {
+
+  constructor(studentId, fname, lname) {
+    super(fname, lname);
+    this.studentId = studentId;
+  }
+
+  getStudentInfo() {
+    return this.studentId + " " + this.lname + ", " + this.fname;
+  }
+
+}
+
+var student = new Student(1, "Ajay","SM");
+console.log(student.getFullName());
+console.log(student.getStudentInfo());
